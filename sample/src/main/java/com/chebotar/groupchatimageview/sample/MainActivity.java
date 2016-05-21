8cf72a70422774fa911faff51f8f50e4e15dd8b2
VikaTsyrulnyk <viktoria.tsyrulnyk@gmail.com>
2016-05-22 00:14:05 +0300

    Refactor to library and sample

---
+++ sample/src/main/java/com/chebotar/groupchatimageview/sample/MainActivity.java
@@ -0,0 +1,15 @@
+package com.chebotar.groupchatimageview.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.chebotar.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
